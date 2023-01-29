7ff6b952fecdf2bfbba77e71fac0f7d973ea2571
chenghao zhang <c962545760@gmail.com>
2023-01-29 20:39:44 +0800

    changed

--- python/dataset.py
+++ python/dataset.py
@@ -13,7 +13,7 @@ import matplotlib.pyplot as plt
 
 '''
 fun:
-      find not empty dictory in default path or input path
+      find dictory with picture in default path or input path
       default path: origin_data_path_w for windows
                     origin_data_path_l for linux
 para:
@@ -61,13 +61,15 @@ def get_dataset_path(**kwargs):
     return {"path":origin_data_path, "dict":dict_name}
 
 '''
-fun:
-      
+fun: generate cls_dataset according to root or input para (option) 
+     called by function 'load_dataset(path, **kwargs)'
 para:
-    print_ctr  -->
+    root       -->
+    (option)
+    print_ctr  -->   
     pic_trans  -->
 return:
-
+    class cls_dataset
 '''
 
 class cls_dataset(Dataset):
@@ -81,6 +83,7 @@ class cls_dataset(Dataset):
             print_flag = 0
 
         self.root = root
+
         if print_flag:
             print(f'path is:', self.root)
             print(f'dictory is:', self.root.split('/')[-1])
@@ -111,14 +114,16 @@ class cls_dataset(Dataset):
 
 '''
 fun:
-      
+    generate cls_dataset
 para:
+    path       -->
+    (option)
     show_info  -->
     show_pic   -->
-    print_ctr  -->
-    pic_trans  -->
+    print_ctr  -->   (used by 'class cls_dataset(Dataset)')
+    pic_trans  -->   (used by 'class cls_dataset(Dataset)')
 return:
-
+    class cls_dataset
 '''
 
 def load_dataset(path, **kwargs):
@@ -128,7 +133,6 @@ def load_dataset(path, **kwargs):
         del kwargs["show_pic"]
     else:
         show_pic = 0
-
 
     if (len(kwargs) > 0) and ("show_info" in kwargs):
         show_info = 1
@@ -146,14 +150,25 @@ def load_dataset(path, **kwargs):
         if (len(kwargs) > 0) and ("index" in kwargs):
             pic_index = kwargs["index"]
             print(pic_index)
+            ### show picture
             for i in range(0, len(pic_index)):
                 p = data_set.__getitem__(pic_index[i])['image']
-                print(p.shape)
+                # print(p.shape)
                 img = ToPILImage()(p)
                 img.show()
 
     return data_set
 
+'''
+fun:
+    generate .hdf5 file according to original picture
+para:
+    root    -->
+    (option)
+    reserve
+return:
+    class cls_dataset
+'''
 
 def create_h5_file(root, **kwargs):
 
@@ -197,7 +212,16 @@ def create_h5_file(root, **kwargs):
     h5_file.close()  
     print(f'finish complete:', save_path.split('/')[-1])  
 
-
+'''
+fun:
+    generate class h5py_dataset
+para:
+    root    -->
+    (option)
+    reserve
+return:
+    class h5py_dataset
+'''
 
 class h5py_dataset(Dataset):
 
@@ -213,42 +237,47 @@ class h5py_dataset(Dataset):
         with h5py.File(self.file, 'r') as f:
             return len(f['image'])
     
-
-
-# def load_dataset_h5(file):
-#     return DataLoader()
-
-# print(path_dict["path"].split('/'))
-
-
-# for i in range(len(path_dict["dict"]) - 2):
-#     # print(os.path.join(path_dict["path"], path_dict["dict"][i]))
-# # data_awgn_training = cls_dataset(
-# #     os.path.join(path_dict["path"], path_dict["dict"][1]), 
-# #     print_ctr = 0)
-
-# data = load_dataset(os.path.join(path_dict["path"], path_dict["dict"][0]), show_pic=1, index=[0])
-#             pic_trans = [transforms.Grayscale()], save=1)
-# print(len(data))
-pic_list = [
-                # transforms.Resize((96 * 2,96 * 2)),
-                # transforms.CenterCrop((400,400)),
-                transforms.ConvertImageDtype(torch.double),
-                transforms.Normalize([0.5,0.5,0.5],[0.5,0.5,0.5]),
-                # transforms.Grayscale()
-            ]
+'''
+exapmle code:
+1. use function 'load_dataset(path, **kwargs)'   ('class cls_dataset(Dataset)')
 
 path_dict = get_dataset_path()
+data_set = load_dataset(os.path.join(path_dict["path"], path_dict["dict"][0]))
+p = data_set.__getitem__(0)['image']
+print(data_set.__len__())
+print(p.shape)       ### for RGB pic: torch.Size([3, x, y])
+print(type(p))       ### type:torch.Tensor
+img = ToPILImage()(p)
+img.show()
+
+2. use 'class h5py_dataset(Dataset)'
+path_dict = get_dataset_path()
+pic_list = []   ### a list of torchvision.transforms.methodxx
 # create_h5_file(os.path.join(path_dict["path"], path_dict["dict"][0]), pic_trans = pic_list)
-# data =  h5py_dataset(os.path.join(path_dict["path"], path_dict["dict"][0]))
+data = h5py_dataset(os.path.join("D:/workspace/art/data_h5", (path_dict["dict"][0] + '.hdf5')))
+imag, data_type = data.__getitem__(0)
+dataset_scale = data.__len__()  
+print(dataset_scale)
+print(imag.shape)      ### for RGB pic: (3, x, y)
+print(type(imag))      ### type:numpy.ndarray
+### show picture
+imag = imag.transpose(1, 2, 0)   ### change (3, x, y) to (x, y, 3)
+print(imag.shape)
+plt.imshow(imag)
+plt.show()   
 
 
-data = h5py_dataset("D:/workspace/art/data_h5/awgn_test.hdf5")
-# data = DataLoader(data)
-# imag, data_type = data.__getitem__(1)
-# imag = imag.transpose(1, 2, 0)
-# print(type(imag))
-# print((data_type))
-# print(imag.shape)
-# plt.imshow(imag)
-# plt.show()
+notice:
+function 'load_dataset(path, **kwargs)' is uesd to generate dataset according to picture directly
+(or use 'class cls_dataset(Dataset)' to get dataset)
+
+the way to show picture is in function 'load_dataset(path, **kwargs)':
+p = data_set.__getitem__(pic_index[i])['image']
+print(p.shape)
+img = ToPILImage()(p)
+img.show()
+
+'class h5py_dataset(Dataset)' is used to get dataset from .hdf5 file
+the way to show picture is complex
+
+'''
