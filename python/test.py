e4a7da626bb6b4a890e7b7a0d8cb0a0a2549468a
chenghao zhang <c962545760@gmail.com>
2023-02-01 11:01:12 +0800

    update python

--- python/test.py
+++ python/test.py
@@ -1,15 +1,29 @@
-
-
+import torch
+from torchvision import transforms
 import os
 import dataset
 from torchvision.transforms import ToPILImage
 import matplotlib.pyplot as plt
-
+import platform
 
 path_dict = dataset.get_dataset_path()
-pic_list = []   ### a list of torchvision.transforms.methodxx
+pic_list = [
+        # transforms.Resize((96 * 2,96 * 2)),
+        # transforms.CenterCrop((400,400)),
+        transforms.ConvertImageDtype(torch.double),
+        transforms.Normalize([0.5,0.5,0.5],[0.5,0.5,0.5]),
+        # transforms.Grayscale()
+    ]  
+### a list of torchvision.transforms.methodxx
 # dataset.create_h5_file(os.path.join(path_dict["path"], path_dict["dict"][0]), pic_trans = pic_list)
-data = dataset.h5py_dataset(os.path.join("/home/jzm/workspace/final/data_h5", (path_dict["dict"][0] + '.hdf5')))
+
+if platform.system() == 'Windows':
+    path = 'D:/workspace/art/data_h5'
+else:
+    path = '/home/jzm/workspace/final/data_h5'
+
+
+data = dataset.h5py_dataset(os.path.join(path, (path_dict["dict"][0] + '.hdf5')))
 imag, data_type = data.__getitem__(0)
 dataset_scale = data.__len__()  
 print(dataset_scale)
@@ -18,5 +32,5 @@ print(type(imag))      ### type:numpy.ndarray
 ### show picture
 imag = imag.transpose(1, 2, 0)   ### change (3, x, y) to (x, y, 3)
 print(imag.shape)
-plt.imshow(imag)
+plt.imshow(imag.astype('uint8'))
 plt.show()  
